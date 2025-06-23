6e428c6bce7292cbe9364f19bf383d4a124dfcc5
akia <ch.royer15@gmail.com>
2025-06-23 13:28:20 +0200

    fix stripe payment

--- handlers/stripe/webhook.go
+++ handlers/stripe/webhook.go
@@ -154,14 +154,15 @@ func handleCheckoutSessionCompleted(c *gin.Context, event stripe.Event) {
 
 	if session.Invoice != nil {
 		utils.LogError(nil, "PaymentIntent présent dans handleCheckoutSessionCompleted")
-		err1 := upsertSubscriptionPayment(sub.ID, int(session.AmountTotal), session.Invoice.ID, models.SubscriptionPaymentPending)
-		if err1 != nil {
-			utils.LogError(err1, "Erreur upsertSubscriptionPayment (pending) dans handleCheckoutSessionCompleted")
-		}
 		if session.PaymentStatus == "paid" {
-			err2 := upsertSubscriptionPayment(sub.ID, int(session.AmountTotal), session.PaymentIntent.ID, models.SubscriptionPaymentSucceeded)
+			err1 := upsertSubscriptionPayment(sub.ID, int(session.AmountTotal), session.PaymentIntent.ID, models.SubscriptionPaymentSucceeded)
+			if err1 != nil {
+				utils.LogError(err1, "Erreur upsertSubscriptionPayment (paid) dans handleCheckoutSessionCompleted")
+			}
+		} else {
+			err2 := upsertSubscriptionPayment(sub.ID, int(session.AmountTotal), session.Invoice.ID, models.SubscriptionPaymentPending)
 			if err2 != nil {
-				utils.LogError(err2, "Erreur upsertSubscriptionPayment (paid) dans handleCheckoutSessionCompleted")
+				utils.LogError(err2, "Erreur upsertSubscriptionPayment (pending) dans handleCheckoutSessionCompleted")
 			}
 		}
 	} else {
